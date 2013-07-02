5a87e0914be83f4a5db3b4e4124e9936479bfcf6
A. Samuel Pottinger <Samnsparky@gmail.com>
2013-07-02 11:22:54 -0600

    Updated documentation in bash example.

--- example_many_input_files.bash
+++ example_many_input_files.bash
@@ -1,3 +1,13 @@
+# Demonstrates how to run many input files with summarizer.py
+# 
+# Simple bash snippet that demonstrates how to run summarizer.py with many
+# input files (many files each with a list of words to summarize) while using
+# the same distance matrix CSV.
+#
+# Author: A. Samuel Pottinger (2013)
+# License: MIT
+#
+
 while read target; do
   echo $target >> combined_values.txt;
   python summarizer.py $target HowellNounDistances.csv >> combined_values.txt;
